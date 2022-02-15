0c12f9e9f2de8d854a6f078111507e5f18d114fc
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-02-15 15:01:28 +0600

    Modify with operation variables & use DB value in variables

--- Variable_part.sql
+++ Variable_part.sql
@@ -8,8 +8,30 @@ select @name;
 select @name  ='Israt';
 select @name;
 
--- Multiple variable 
+-- Multiple variable with  Alaise
 
-declare @myname varchar(50), @age int;
+declare @myname varchar(50), @age int , @salary decimal (7,2)=45000;
 select @myname = 'Lelin',@age = 25;
-select @myname , @age;
+select @myname as 'Name' , @age as 'Age' , @salary as 'Salary'; -- Alaise for column name use as keyword
+
+
+--Operation with variable
+
+declare @number int = 100;
+set @number+= 50;
+select @number as 'Number';
+
+set @number = @number-50;
+select @number as 'Number';
+
+select * from employee_info;
+ --Assign value from DB data
+declare @name varchar(50) , @salary decimal (7,2);
+select @name = empName , @salary = empSalary from   employee_info;
+select @name as 'Name' , @salary as 'Salary';
+
+
+
+
+
+
